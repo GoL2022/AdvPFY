594d37579b1a7df6ecdf73da6735ec4963c048fa
Zhaoyuan Yang <zhyyang1015@gmail.com>
2025-01-12 16:27:38 -0500

    add cifar10 model arch

--- model/nn_model.py
+++ model/nn_model.py
@@ -1,7 +1,22 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision.models.resnet import BasicBlock, resnet18, resnet50
 
+def init_params(models):
+    if not isinstance(models, list):
+        models = [models]
+
+    for model in models:
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d) \
+                    or isinstance(m, nn.Linear) \
+                    or isinstance(m, nn.ConvTranspose2d):
+                nn.init.kaiming_normal_(m.weight)
+                nn.init.normal_(m.bias) if (m.bias is not None) else None
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
 class BasicBlock(nn.Module):
     def __init__(self, c_in, c_out, num_feature_map=64):
@@ -125,3 +140,81 @@ class StAEClassifier(nn.Module):
             return y
         else:
             return x, z, y
+
+
+class ResNetEnc(nn.Module):
+    def __init__(self, image_size=32, **kwargs):
+        super(ResNetEnc, self).__init__()
+        self.image_size = image_size
+        self.model = resnet50(pretrained=True)
+
+        self.model.avgpool = nn.AdaptiveAvgPool2d(output_size=1)
+        if self.image_size == 32:
+            self.model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+            delattr(self.model, 'maxpool')
+            init_params(self.model.conv1)
+
+    def forward(self, x):
+        x = self.model.conv1(x)
+        x = self.model.bn1(x)
+        x = self.model.relu(x)
+        # if hasattr(self.model, 'maxpool'): x = self.model.maxpool(x)
+        x = self.model.layer1(x)
+        x = self.model.layer2(x)
+        x = self.model.layer3(x)
+        x = self.model.layer4(x)
+        x = self.model.avgpool(x)
+        x = x.view(x.size(0), -1)
+        return x
+
+        
+class ResNetVAE(nn.Module):
+    def __init__(self, REncoder, latent_dim=256):
+        super(ResNetVAE, self).__init__()
+        self.encoder_module = nn.ModuleList([REncoder])
+        self.decoder_module = nn.ModuleList([BasicBlock(latent_dim, latent_dim), 
+                                             BasicBlock(latent_dim, latent_dim), 
+                                             BasicBlock(latent_dim, latent_dim),
+                                             BasicBlock(latent_dim, latent_dim)])
+        
+        self.output_layer = nn.ModuleList([BasicBlock(latent_dim, 8), BasicBlock(8, 3)])
+        self.output_layer2 = nn.Linear(latent_dim, 10)
+        self.mean_val = nn.Linear(latent_dim * 8, latent_dim)
+        self.var_val = nn.Linear(latent_dim * 8, latent_dim)
+        self.latent_dim = latent_dim
+        
+    def forward(self, x, deterministic=True, classification_only=True):
+        for i_op in self.encoder_module:
+            x = i_op(x)
+            x = nn.ReLU()(x)
+        
+        # encoder latent weight     
+        mu = self.mean_val(x)
+        logvar = self.var_val(x)
+        
+        if deterministic:
+            sample_z = mu
+        else:
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)
+            sample_z = eps * std + mu
+        
+        x = sample_z[:,:,None,None]
+        z = x
+        
+        y = self.output_layer2(torch.mean(z, dim=(2, 3)))
+        x = nn.Upsample(scale_factor=2, mode='nearest')(x)
+        for i_op in self.decoder_module:
+            x = i_op(x)
+            # x = nn.ReLU()(x)
+            x = nn.Upsample(scale_factor=2, mode='nearest')(x)
+        
+        for i_op in self.output_layer:
+            # x = nn.ReLU()(x)
+            x = i_op(x)
+        
+        x = torch.tanh(x) 
+        if classification_only:
+            return y
+        else:
+            return x, z, y, mu, logvar
